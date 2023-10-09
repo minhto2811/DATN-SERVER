97377bc284fd25883851114c4e3bec044883b866
Minh Tô <milkissily@gmail.com>
2023-10-09 19:16:12 +0700

    setup css

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 const path = require('path')
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, '/src/public')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
