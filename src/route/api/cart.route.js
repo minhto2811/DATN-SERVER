a176e0ca79244f70b2b264491652df015c88cdf3
Minh Tô <128146974+minhto2811@users.noreply.github.com>
2023-11-27 08:13:31 +0700

    new

--- src/route/api/cart.route.js
+++ src/route/api/cart.route.js
@@ -5,7 +5,7 @@ const { checkUser } = require('../../midleware/authentication')
 
 
 router.post('/add', checkUser, controller.add)
-router.post('/get-all', checkUser, controller.getAll)
+router.get('/get-all', checkUser, controller.getAll)
 router.post('/delete', checkUser, controller.delete)
 router.post('/update', checkUser, controller.update)
 
