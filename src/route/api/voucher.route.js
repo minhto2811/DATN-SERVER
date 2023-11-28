1f7538afdb4d81723da60b3e2c2f7cb38d8b1cf4
Minh Tô <128146974+minhto2811@users.noreply.github.com>
2023-11-28 16:48:45 +0700

    ok

--- src/route/api/voucher.route.js
+++ src/route/api/voucher.route.js
@@ -5,7 +5,7 @@ const {checkUser} = require('../../midleware/authentication')
 
 router.get('/get-all', controller.getAll)// lấy voucher không cần đăng nhập
 router.get('/get-by-user', checkUser,controller.getAllByUser)// lấy tất cả voucher trên hệ thống mà người dùng chưa thêm
-router.post('/add',  controller.add)
+router.post('/add',checkUser,  controller.add)
 router.get('/get', checkUser, controller.get)// lấy voucher của user
 
 router.post('/create',  controller.create)
