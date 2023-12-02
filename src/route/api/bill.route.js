0fe83bdcee225a851fe2decd33863b9df00e8f00
Minh Tô <128146974+minhto2811@users.noreply.github.com>
2023-12-02 19:44:28 +0700

    up

--- src/route/api/bill.route.js
+++ src/route/api/bill.route.js
@@ -4,9 +4,9 @@ const controller = require('../../controller/api/bill.controller')
 const {checkUser} = require('../../midleware/authentication')
 
 router.post('/create', checkUser, controller.createBill)
-router.post('/detail/:id', checkUser, controller.detail)
-router.post('/get-all',checkUser,controller.getAll)
-router.post('/:status', checkUser, controller.getByStatus)
+router.get('/detail/:id', checkUser, controller.detail)
+router.get('/get-all',checkUser,controller.getAll)
+router.get('/:status', checkUser, controller.getByStatus)
 router.post('/cancel', checkUser, controller.cancelBill)
 
 
