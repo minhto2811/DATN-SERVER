02aef0300c1ab0593e55964858ee41c2a7f236f0
mgok <minhto28.dev@gmail.com>
2023-12-17 23:24:51 +0700

    image

--- src/model/product.js
+++ src/model/product.js
@@ -6,7 +6,7 @@ const Product = new Schema(
     product_name: { type: String, require: true },
     brand_id: { type: String, require: true },
     percent_discount: { type: Number, default: 0 },
-    image_preview: { type: String },
+    image_preview: { type: String, default: "https://firebasestorage.googleapis.com/v0/b/shopping-6b085.appspot.com/o/default%2Fnew-product.png?alt=media&token=9ddcc893-c9c4-4fb1-9775-0a45093f50b8" },
     vote: { type: Number, default: 0 },
     sold: { type: Number, default: 0 },
     time: { type: Date, default: Date.now },
